b119e97c6f0af87a17ef40b421ed3434832eafaa
Ashwin1002 <ashwin.shrestha2258@gmail.com>
2023-03-10 16:50:33 +0545

    bugs fixed

--- lib/gantt_chart.dart
+++ lib/gantt_chart.dart
@@ -78,7 +78,7 @@ class GanttChartScreenState extends State<GanttChartScreen> {
                 return GanttChart(
                   fromDate: DateTime.parse(indexItem.startDate),
                   toDate: DateTime.parse(indexItem.endDate),
-                  tasksList: tasksList,
+                  tasksList: tasksList[index],
                   taskColor: Color(colorInt),
                   taskName: indexItem.text,
                   completePercent: indexItem.progress,
@@ -104,7 +104,7 @@ class GanttChartScreenState extends State<GanttChartScreen> {
 class GanttChart extends StatelessWidget {
   final DateTime fromDate;
   final DateTime toDate;
-  final List<TasksDataModel> tasksList;
+  final TasksDataModel tasksList;
   final String taskName;
   final Color taskColor;
   final int completePercent;
@@ -166,24 +166,34 @@ class GanttChart extends StatelessWidget {
   }
 
   List<Widget> buildChartBars(
-      List<TasksDataModel> data, double chartViewWidth, BuildContext context) {
+      TasksDataModel dataItem, double chartViewWidth, BuildContext context) {
     List<Widget> chartBars = [];
 
-    for (int i = 0; i < data.length; i++) {
-      for (int index = 0; index < data[i].childs.length; index++) {
-        debugPrint("child length => ${data[i].childs.length}");
+    for (int index = 0; index < dataItem.childs.length; index++) {
+      final childIndex = dataItem.childs[index];
 
-        final childIndex = data[i].childs[index];
+      debugPrint(" 1st child index loop => $index");
 
-        var remainingWidth = calculateRemainingWidth(
-            DateTime.parse(childIndex.startDate),
-            DateTime.parse(childIndex.endDate));
+      debugPrint(
+          " 1st child list length => ${dataItem.childs.length}, parent id => ${dataItem.childs[index].parent}");
 
-        if (remainingWidth > 0) {
-          final intColor = childIndex.color.replaceAll("#", "0xff");
+      debugPrint("=================================================");
 
-          final Color cColor = Color(int.parse(intColor));
+      // if(childIndex.parent == dataItem.id) {
+      var remainingWidth = calculateRemainingWidth(
+          DateTime.parse(childIndex.startDate),
+          DateTime.parse(childIndex.endDate));
 
+      if (remainingWidth > 0) {
+        final intColor = childIndex.color.replaceAll("#", "0xff");
+
+        final Color cColor = Color(int.parse(intColor));
+
+        debugPrint("parent id => ${dataItem.id}");
+
+        debugPrint("child id => ${childIndex.parent}");
+
+        if (dataItem.id == childIndex.parent) {
           chartBars.add(
             Stack(
               children: [
@@ -205,12 +215,11 @@ class GanttChart extends StatelessWidget {
                           DateTime.parse(childIndex.startDate)) *
                           chartViewWidth /
                           viewRangeToFitScreen,
-                      top: i == 0 ? 4.0 : 2.0,
-                      bottom: i == childIndex.childs.length - 1 ? 4.0 : 2.0
-                  ),
-                  width: (remainingWidth *
-                      chartViewWidth /
-                      viewRangeToFitScreen) *
+                      top: dataItem == 0 ? 4.0 : 2.0,
+                      bottom:
+                      dataItem == childIndex.childs.length - 1 ? 4.0 : 2.0),
+                  width:
+                  (remainingWidth * chartViewWidth / viewRangeToFitScreen) *
                       double.parse(childIndex.progress.toString()) /
                       100,
                   decoration: BoxDecoration(
@@ -227,7 +236,8 @@ class GanttChart extends StatelessWidget {
                 ),
                 Container(
                   decoration: BoxDecoration(
-                      color: cColor.withAlpha(100), borderRadius: BorderRadius.circular(10.0)),
+                      color: cColor.withAlpha(100),
+                      borderRadius: BorderRadius.circular(10.0)),
                   height: 25.0,
                   width: remainingWidth * chartViewWidth / viewRangeToFitScreen,
                   margin: EdgeInsets.only(
@@ -235,8 +245,9 @@ class GanttChart extends StatelessWidget {
                           DateTime.parse(childIndex.startDate)) *
                           chartViewWidth /
                           viewRangeToFitScreen,
-                      top: i == 0 ? 4.0 : 2.0,
-                      bottom: i == data[i].childs.length - 1 ? 4.0 : 2.0),
+                      top: dataItem == 0 ? 4.0 : 2.0,
+                      bottom:
+                      dataItem == dataItem.childs.length - 1 ? 4.0 : 2.0),
                   alignment: Alignment.centerLeft,
                   child: Padding(
                     padding: const EdgeInsets.only(left: 8.0),
@@ -251,7 +262,11 @@ class GanttChart extends StatelessWidget {
               ],
             ),
           );
+        }
 
+        debugPrint("char bar length => ${chartBars.length}");
+
+        if (childIndex.childs.isNotEmpty) {
           for (int cIndex = 0; cIndex < childIndex.childs.length; cIndex++) {
             debugPrint(
                 "child inside child length => ${childIndex.childs.length}");
@@ -287,16 +302,17 @@ class GanttChart extends StatelessWidget {
                             DateTime.parse(childItem.startDate)) *
                             chartViewWidth /
                             viewRangeToFitScreen,
-                        top: i == 0 ? 4.0 : 2.0,
-                        bottom: i == childIndex.childs.length - 1 ? 4.0 : 2.0
-                    ),
+                        top: dataItem == 0 ? 4.0 : 2.0,
+                        bottom: dataItem == childIndex.childs.length - 1
+                            ? 4.0
+                            : 2.0),
                     width: (remainingChildWidth *
                         chartViewWidth /
                         viewRangeToFitScreen) *
                         double.parse(completePercent.toString()) /
                         100,
                     decoration: BoxDecoration(
-                      color: cColor.withOpacity(0.9),
+                      color: cColor.withOpacity(0.4),
                       borderRadius: BorderRadius.circular(10.0),
                       boxShadow: [
                         BoxShadow(
@@ -321,8 +337,10 @@ class GanttChart extends StatelessWidget {
                             DateTime.parse(childItem.startDate)) *
                             chartViewWidth /
                             viewRangeToFitScreen,
-                        top: i == 0 ? 4.0 : 2.0,
-                        bottom: i == childIndex.childs.length - 1 ? 4.0 : 2.0),
+                        top: dataItem == 0 ? 4.0 : 2.0,
+                        bottom: dataItem == childIndex.childs.length - 1
+                            ? 4.0
+                            : 2.0),
                     alignment: Alignment.centerLeft,
                     child: Padding(
                       padding: const EdgeInsets.only(left: 8.0),
@@ -330,7 +348,8 @@ class GanttChart extends StatelessWidget {
                         childItem.text,
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(fontSize: 12.0, color: Colors.white),
+                        style: const TextStyle(
+                            fontSize: 12.0, color: Colors.black),
                       ),
                     ),
                   ),
@@ -340,8 +359,8 @@ class GanttChart extends StatelessWidget {
           }
         }
       }
+      // }
     }
-
     return chartBars;
   }
 
@@ -416,79 +435,110 @@ class GanttChart extends StatelessWidget {
     );
   }
 
-  Widget buildChartForEachUser(List<TasksDataModel> userData,
-      double chartViewWidth, BuildContext context) {
+  Widget buildChartForEachUser(
+      TasksDataModel userData, double chartViewWidth, BuildContext context) {
     // Color color = randomColorGenerator();
 
     var chartBars = buildChartBars(userData, chartViewWidth, context);
 
     return Container(
-        margin: const EdgeInsets.only(bottom: 10.0),
+        margin: const EdgeInsets.only(bottom: 12.0),
         decoration: BoxDecoration(
             border: Border.all(color: Colors.black.withAlpha(90))),
         child: Column(
           children: [
             Stack(
-              alignment: Alignment.centerLeft,
+              alignment: Alignment.centerRight,
               children: [
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: taskColor.withAlpha(100),
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.black.withAlpha(70),
+                Stack(
+                  alignment: Alignment.centerLeft,
+                  children: [
+                    Container(
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: taskColor.withAlpha(100),
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.black.withAlpha(70),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    Container(
+                      height: 40,
+                      width: MediaQuery.of(context).size.width *
+                          double.parse(completePercent.toString()) /
+                          100,
+                      decoration: BoxDecoration(
+                          color: taskColor.withOpacity(0.9),
+                          // borderRadius: BorderRadius.only(
+                          //     topRight: Radius.circular(12.0),
+                          //     bottomRight: Radius.circular(12.0)),
+                          boxShadow: [
+                            BoxShadow(
+                                color: taskColor.withOpacity(0.4),
+                                spreadRadius: 4,
+                                blurRadius: 9,
+                                offset: const Offset(0, 2))
+                          ]),
+                    ),
+                    Center(
+                      child: RotatedBox(
+                        quarterTurns:
+                        chartBars.length * 29.0 + 4.0 > 50 ? 0 : 0,
+                        child: Column(
+                          children: [
+                            Text(
+                              taskName,
+                              textAlign: TextAlign.center,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  fontSize: 12.5, fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(height: 3.0),
+                            Text(
+                              "${EasyDateConverter.nepaliDateFormatToHalfMonthDay(date: EasyDateConverter.englishDateToNepaliDate(date: fromDate.toString()))} to ${EasyDateConverter.nepaliDateFormatToHalfMonthDay(date: EasyDateConverter.englishDateToNepaliDate(date: toDate.toString()))} (${toDate.difference(fromDate).inDays} days)",
+                              textAlign: TextAlign.center,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(fontSize: 10.5),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                Container(
-                  height: 40,
-                  width: MediaQuery.of(context).size.width *
-                      double.parse(completePercent.toString()) /
-                      100,
-                  decoration: BoxDecoration(
-                      color: taskColor.withOpacity(0.9),
-                      // borderRadius: BorderRadius.only(
-                      //     topRight: Radius.circular(12.0),
-                      //     bottomRight: Radius.circular(12.0)),
-                      boxShadow: [
-                        BoxShadow(
-                            color: taskColor.withOpacity(0.4),
-                            spreadRadius: 4,
-                            blurRadius: 9,
-                            offset: const Offset(0, 2))
-                      ]),
-                ),
-                Center(
-                  child: RotatedBox(
-                    quarterTurns: chartBars.length * 29.0 + 4.0 > 50 ? 0 : 0,
-                    child: Column(
+                if(tasksList.status == "1")
+                  Container(
+                    width: 80,
+                    margin: EdgeInsets.only(right: 4.0),
+                    padding: const EdgeInsets.all(4.0),
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(12.0)
+                    ),
+                    child: Row(
                       children: [
-                        Text(
-                          taskName,
-                          textAlign: TextAlign.center,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              fontSize: 12.5, fontWeight: FontWeight.w500),
+                        Icon(
+                          Icons.check_circle_outline,
+                          size: 16.0,
+                          color: Colors.white,
                         ),
-                        SizedBox(height: 3.0),
+                        const SizedBox(width: 3.0),
                         Text(
-                          "${EasyDateConverter.nepaliDateFormatToHalfMonthDay(date: EasyDateConverter.englishDateToNepaliDate(date: fromDate.toString()))} to ${EasyDateConverter.nepaliDateFormatToHalfMonthDay(date: EasyDateConverter.englishDateToNepaliDate(date: toDate.toString()))} (${toDate.difference(fromDate).inDays} days)",
-                          textAlign: TextAlign.center,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(fontSize: 10.5),
-                        ),
+                          "Completed",
+                          style: TextStyle(fontSize: 9.5,
+                              color: Colors.white),
+                        )
                       ],
                     ),
                   ),
-                ),
               ],
             ),
             SizedBox(
-              height: chartBars.length * 29.0 + 25.0 + chartBars.length * 2,
+              height: chartBars.length * 29.0 + 25.0 + chartBars.length * 12,
               child: SingleChildScrollView(
                 physics: const BouncingScrollPhysics(),
                 scrollDirection: Axis.horizontal,
@@ -515,18 +565,16 @@ class GanttChart extends StatelessWidget {
   List<Widget> buildChartContent(double chartViewWidth, BuildContext context) {
     List<Widget> chartContent = [];
 
-    for (var user in tasksList) {
-      List<TasksDataModel> projectsOfUser = [];
-
-      projectsOfUser = tasksList
-          .where(
-              (project) => project.id.toString().contains(user.id.toString()))
-          .toList();
-
-      if (projectsOfUser.isNotEmpty) {
-        debugPrint("total widgets length => ${chartContent.length}");
-      }
-    }
+    // for (var user in tasksList) {
+    //   List<TasksDataModel> projectsOfUser = [];
+    //
+    //   // projectsOfUser = tasksList
+    //   //     .where(
+    //   //         (project) => project.id.toString().contains(user.id.toString()))
+    //   //     .toList();
+    //
+    //   if (projectsOfUser.isNotEmpty) {}
+    // }
 
     chartContent.add(buildChartForEachUser(tasksList, chartViewWidth, context));
     return chartContent;
